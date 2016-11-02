edacb57d42555c72513c43f5c2adda78551bdaa0
Denis Novikov <denisnovikov@garant.ru>
2016-11-02 12:11:54 +0300

    Remove useless check
     Since the `s` variable was checked, there is no need to recheck it once
     more.

--- wsf_c/axis2c/util/src/platforms/unix/uuid_gen_unix.c
+++ wsf_c/axis2c/util/src/platforms/unix/uuid_gen_unix.c
@@ -205,10 +205,6 @@ axutil_platform_uuid_gen(
         return NULL;
     }
     uuid_str = s;
-    if(!uuid_str)
-    {
-        return NULL;
-    }
     memcpy(mac, uuid_struct->mac_addr, 6);
     sprintf(mac_hex, "%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     sprintf(uuid_str, "%08x-%04x-%04x-%04x-%s", uuid_struct->time_low, uuid_struct->time_mid,
